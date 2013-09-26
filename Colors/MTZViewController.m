cf65ca495647afa24f9debbb3d7b37325f035e44
Matt Zanchelli <matt@mdznr.com>
2013-09-26 03:26:15 -0400

    Playback controls work

--- Colors/MTZViewController.m
+++ Colors/MTZViewController.m
@@ -36,6 +36,8 @@
 @property (strong, nonatomic) IBOutlet UILabel *timeElapsed;
 @property (strong, nonatomic) IBOutlet UILabel *timeRemaining;
 
+@property (strong, nonatomic) IBOutlet UIButton *playPause;
+
 @end
 
 @implementation MTZViewController
@@ -53,19 +55,37 @@
                            selector:@selector(songChanged:)
                                name:MPMusicPlayerControllerNowPlayingItemDidChangeNotification
                              object:_player];
+	[notificationCenter addObserver:self
+						   selector:@selector(playbackStateDidChange:)
+							   name:MPMusicPlayerControllerPlaybackStateDidChangeNotification
+							 object:_player];
+	[notificationCenter addObserver:self
+						   selector:@selector(volumeDidChange:)
+							   name:MPMusicPlayerControllerVolumeDidChangeNotification
+							 object:_player];
     [_player beginGeneratingPlaybackNotifications];
+	
+	[self checkPlaybackStatus];
 	
 	_trackSlider.fillImage = [UIImage imageNamed:@"ProgressFill"];
 	_trackSlider.trackImage = [UIImage imageNamed:@"ProgressTrack"];
 	[_trackSlider setThumbImage:[UIImage imageNamed:@"ProgressThumb"]
 					   forState:UIControlStateNormal];
-	_trackSlider.value = 0.5f;
 	
+	_volumeSlider.value = _player.volume;
 	_volumeSlider.fillImage = [UIImage imageNamed:@"VolumeFill"];
 	_volumeSlider.trackImage = [UIImage imageNamed:@"VolumeTrack"];
 	[_volumeSlider setThumbImage:[UIImage imageNamed:@"VolumeThumb"]
 						forState:UIControlStateNormal];
-	_volumeSlider.value = 0.5f;
+}
+
+- (void)checkPlaybackStatus
+{
+	if ( _player.playbackState == MPMusicPlaybackStatePlaying ) {
+		[_playPause setImage:[UIImage imageNamed:@"Pause"] forState:UIControlStateNormal];
+	} else {
+		[_playPause setImage:[UIImage imageNamed:@"Play"] forState:UIControlStateNormal];
+	}
 }
 
 - (void)songChanged:(id)sender
@@ -95,6 +115,26 @@
 	_timeRemaining.text = [NSString stringWithFormat:@"-%.0f:%.0f", minutes, seconds];
 }
 
+- (void)playbackStateDidChange:(id)sender
+{
+	switch ( _player.playbackState ) {
+		case MPMusicPlaybackStatePlaying:
+			[_playPause setImage:[UIImage imageNamed:@"Pause"] forState:UIControlStateNormal];
+			break;
+		case MPMusicPlaybackStateStopped:
+			[_player stop];
+		case MPMusicPlaybackStatePaused:
+			[_playPause setImage:[UIImage imageNamed:@"Play"] forState:UIControlStateNormal];
+		default:
+			break;
+	}
+}
+
+- (void)volumeDidChange:(id)sender
+{
+	_volumeSlider.value = _player.volume;
+}
+
 - (void)refreshColors
 {
 #warning animate this change? Animate the change of album art (if it changes), too?
@@ -104,6 +144,37 @@
 #warning why do I have to change this myself?
 	_trackSlider.tintColor = bg;
 	_volumeSlider.tintColor = bg;
+}
+
+- (IBAction)playPause:(id)sender
+{
+	if ( _player.playbackState == MPMusicPlaybackStatePlaying ) {
+        [_player pause];
+    } else {
+        [_player play];
+    }
+}
+
+- (IBAction)fastForward:(id)sender
+{
+	[_player skipToNextItem];
+}
+
+- (IBAction)rewind:(id)sender
+{
+#warning go to previous item if in the first few seconds of playback else, go to beginning of song
+	[_player skipToPreviousItem];
+}
+
+- (IBAction)volumeChanged:(id)sender
+{
+	_player.volume = _volumeSlider.value;
+}
+
+- (void)dealloc
+{
+#warning unregister notifications?
+	[_player endGeneratingPlaybackNotifications];
 }
 
 - (void)didReceiveMemoryWarning
