041bbd30752080e588c10827875fea9a896f9f7c
Matt Zanchelli <matt@mdznr.com>
2013-10-28 23:09:43 -0400

    Simple UI set up to check contrast calculated via LAB colorspace.

--- Colors/MTZViewController.m
+++ Colors/MTZViewController.m
@@ -8,8 +8,6 @@
 
 #import "MTZViewController.h"
 
-@import MediaPlayer;
-
 #import "UIColor+Components.h"
 #import "UIColor+Hex.h"
 #import "UIColor+Manipulation.h"
@@ -23,6 +21,19 @@
 
 @interface MTZViewController ()
 
+@property (strong, nonatomic) IBOutlet UIView *color1Viewer;
+@property (strong, nonatomic) IBOutlet UIView *color2Viewer;
+
+@property (strong, nonatomic) IBOutlet UILabel *colorContrastLabel;
+
+@property (strong, nonatomic) IBOutlet UISlider *rSlider1;
+@property (strong, nonatomic) IBOutlet UISlider *gSlider1;
+@property (strong, nonatomic) IBOutlet UISlider *bSlider1;
+
+@property (strong, nonatomic) IBOutlet UISlider *rSlider2;
+@property (strong, nonatomic) IBOutlet UISlider *gSlider2;
+@property (strong, nonatomic) IBOutlet UISlider *bSlider2;
+
 @end
 
 @implementation MTZViewController
@@ -31,6 +42,32 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)sliderValueChanged:(id)sender
+{
+	[self refreshColors];
+}
+
+- (void)refreshColors
+{
+	UIColor *color1 = [UIColor colorWithRed:_rSlider1.value/255.0f
+									  green:_gSlider1.value/255.0f
+									   blue:_bSlider1.value/255.0f
+									  alpha:1.0f];
+	_color1Viewer.backgroundColor = color1;
+	_rSlider1.tintColor = color1;
+	_gSlider1.tintColor = color1;
+	_bSlider1.tintColor = color1;
+	
+	UIColor *color2 = [UIColor colorWithRed:_rSlider2.value/255.0f
+									  green:_gSlider2.value/255.0f
+									   blue:_bSlider2.value/255.0f
+									  alpha:1.0f];
+	_color2Viewer.backgroundColor = color2;
+	_rSlider2.tintColor = color2;
+	_gSlider2.tintColor = color2;
+	_bSlider2.tintColor = color2;
 }
 
 #pragma mark View Controller end
