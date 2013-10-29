26497e495f0526886f5192e278cd070dddcceed7
Matt Zanchelli <matt@mdznr.com>
2013-10-29 15:35:38 -0400

    Added stub for color finder

---
+++ Colors/UIColor+colorFinder.m
@@ -0,0 +1,18 @@
+//
+//  UIColor+colorFinder.m
+//  Colors
+//
+//  Created by Matt on 10/29/13.
+//  Copyright (c) 2013 Matt Zanchelli. All rights reserved.
+//
+
+#import "UIColor+colorFinder.h"
+
+@implementation UIColor (colorFinder)
+
+- (UIColor *)closestColorInSet:(NSSet *)colors
+{
+	
+}
+
+@end
