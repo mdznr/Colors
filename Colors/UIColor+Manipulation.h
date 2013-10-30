ffc129bbdf6b5ea2aeb6d0adb10fa87bbcd737c7
Matt Zanchelli <matt@mdznr.com>
2013-10-30 00:26:00 -0400

    Removed euclidean distance method from header

--- Colors/UIColor+Manipulation.h
+++ Colors/UIColor+Manipulation.h
@@ -10,10 +10,6 @@
 
 @interface UIColor (Manipulation)
 
-/// Calculate the Euclidean Distance between colors in the LAB colorspace.
-+ (float)euclideanDistanceFromColor:(UIColor *)color
-							toColor:(UIColor *)anotherColor;
-
 /// The perceived difference between two colors.
 /// Calculated using the CIEDE2000 Color-Difference Formula.
 + (CGFloat)differenceBetweenColor:(UIColor *)color1 andColor:(UIColor *)color2;
