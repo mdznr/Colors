6c7010fbf6ef32dd3b88ade47daae11dd51130b6
Matt Zanchelli <matt@mdznr.com>
2013-10-30 21:37:46 -0400

    Merging documentation from master

--- Colors/CGRectManipulation.h
+++ Colors/CGRectManipulation.h
@@ -7,4 +7,8 @@
 
 @import Foundation;
 
+/// Returns a rectangle with an origin that is offset from that of the source rectangle.
+/// @param rect The source rectangle.
+/// @param scale The scale value to multiply the dimensions of the rect.
+/// @return A rectangle that is at the same origin as the source, but with its width and height multiplied by the scale.
 CGRect CGRectScale(CGRect rect, CGFloat scale);
