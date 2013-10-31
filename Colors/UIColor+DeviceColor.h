27c76ec1156c7bf18feb0d2bc1fc3a82fe2a117d
Matt Zanchelli <matt@mdznr.com>
2013-10-30 21:53:05 -0400

    Created stubs for getting app icon color, device color, and user wallpaper color.

---
+++ Colors/UIColor+DeviceColor.h
@@ -0,0 +1,19 @@
+//
+//  UIColor+DeviceColor.h
+//  Colors
+//
+//  Created by Matt on 10/30/13.
+//  Copyright (c) 2013 Matt Zanchelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (DeviceColor)
+
++ (UIColor *)appIconColor;
+
++ (UIColor *)deviceColor;
+
++ (UIColor *)userWallpaperColor;
+
+@end
---
+++ Colors/UIColor+DeviceColor.m
@@ -0,0 +1,32 @@
+//
+//  UIColor+DeviceColor.m
+//  Colors
+//
+//  Created by Matt on 10/30/13.
+//  Copyright (c) 2013 Matt Zanchelli. All rights reserved.
+//
+
+#import "UIColor+DeviceColor.h"
+#import "UIColor+NeueColors.h"
+
+@implementation UIColor (DeviceColor)
+
++ (UIColor *)appIconColor
+{
+#warning TODO: Implement
+	return nil;
+}
+
++ (UIColor *)deviceColor
+{
+#warning TODO: Implement
+	return nil;
+}
+
++ (UIColor *)userWallpaperColor
+{
+#warning TODO: Implement
+	return nil;
+}
+
+@end
