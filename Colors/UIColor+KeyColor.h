813a2e48c5c2fffca1543bebef1d8fa4adec104b
Matt Zanchelli <matt@mdznr.com>
2013-10-31 01:46:35 -0400

    Making note that return values of isKeyColorAppropriate might not be consistent. Using @import instead of #import

--- Colors/UIColor+KeyColor.h
+++ Colors/UIColor+KeyColor.h
@@ -6,13 +6,13 @@
 //  Copyright (c) 2013 Matt Zanchelli. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 
 @interface UIColor (KeyColor)
 
 /// A Boolean value determining whether the receiving color would be appropriate to use as a key color.
 /// @returns A Boolean value determing whether the receiving color would be appropriate to use as a key color.
-/// @discussion This can be used to determine whether or not to use a color as the tintColor for an enabled user interface control. 
+/// @discussion This can be used to determine whether or not to use a color as the tintColor for an enabled user interface control. Do not rely on this to return the same value all of the time. Results may vary depending on user settings and ambient conditions. (Do not store the return value for an extended period of time).
 - (BOOL)isKeyColorAppropriate;
 
 /// Find the first color in the provided array that is key color appropriate.
