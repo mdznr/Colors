ea0b96809e42b383de69cb722a045f69157261c2
Matt Zanchelli <matt@mdznr.com>
2013-11-02 23:05:09 -0400

    Setting up UI for contrast against Greyscale colors for testing for Key Colors.

---
+++ Colors/MTZColorSpaceView.h
@@ -0,0 +1,13 @@
+//
+//  MTZColorSpaceView.h
+//  Colors
+//
+//  Created by Matt Zanchelli on 11/2/13.
+//  Copyright (c) 2013 Matt Zanchelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTZColorSpaceView : UIView
+
+@end
---
+++ Colors/MTZColorSpaceView.m
@@ -0,0 +1,31 @@
+//
+//  MTZColorSpaceView.m
+//  Colors
+//
+//  Created by Matt Zanchelli on 11/2/13.
+//  Copyright (c) 2013 Matt Zanchelli. All rights reserved.
+//
+
+#import "MTZColorSpaceView.h"
+
+@implementation MTZColorSpaceView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
