c9110915e003f0617d05dffa659500813a7429e9
Matt Zanchelli <matt@mdznr.com>
2013-11-07 00:18:05 -0500

    Specifying that iOS 5.0 or higher is required for UIColor+Components category methods

--- Colors/UIColor+Components.h
+++ Colors/UIColor+Components.h
@@ -12,45 +12,48 @@
 
 #pragma mark - RGB
 /// Returns the receiver’s red component (RGB).
-- (CGFloat)redComponent;
+/// @returns A float value of the receiver’s red component in the RGB colorspace. May return -1 if unable to convert to RGB.
+- (CGFloat)redComponent NS_AVAILABLE_IOS(5_0);
 /// Returns the receiver's green component (RGB).
-- (CGFloat)greenComponent;
+/// @returns A float value of the receiver’s green component in the RGB colorspace. May return -1 if unable to convert to RGB.
+- (CGFloat)greenComponent NS_AVAILABLE_IOS(5_0);
 /// Returns the receiver's blue component (RGB).
-- (CGFloat)blueComponent;
+/// @returns A float value of the receiver’s blue component in the RGB colorspace. May return -1 if unable to convert to RGB.
+- (CGFloat)blueComponent NS_AVAILABLE_IOS(5_0);
 
 #pragma mark - CMYK
 /// Returns the receiver's cyan component (CMYK).
-- (CGFloat)cyanComponent;
+- (CGFloat)cyanComponent NS_AVAILABLE_IOS(5_0);
 /// Returns the receiver's magenta component (CMYK).
-- (CGFloat)magentaComponent;
+- (CGFloat)magentaComponent NS_AVAILABLE_IOS(5_0);
 /// Returns the receiver's yellow component (CMYK).
-- (CGFloat)yellowComponent;
+- (CGFloat)yellowComponent NS_AVAILABLE_IOS(5_0);
 /// Returns the receiver's black component (CMYK).
-- (CGFloat)blackComponent;
+- (CGFloat)blackComponent NS_AVAILABLE_IOS(5_0);
 
 #pragma mark - HSB
 /// Returns the receiver's hue component (HSB).
-- (CGFloat)hue;
+- (CGFloat)hue NS_AVAILABLE_IOS(5_0);
 /// Returns the receiver's saturation component (HSB).
-- (CGFloat)saturation;
+- (CGFloat)saturation NS_AVAILABLE_IOS(5_0);
 /// Returns the receiver's brightness component (HSB).
-- (CGFloat)brightness;
+- (CGFloat)brightness NS_AVAILABLE_IOS(5_0);
 
 #pragma mark - YUV
 /// Returns the receiver's luma (Y) component (YUV).
-- (CGFloat)yValue;
+- (CGFloat)yValue NS_AVAILABLE_IOS(5_0);
 /// Returns one of the receiver's chrominance (U) components (YUV).
-- (CGFloat)uValue;
+- (CGFloat)uValue NS_AVAILABLE_IOS(5_0);
 /// Returns one of the receiver's chrominance (V) components (YUV).
-- (CGFloat)vValue;
+- (CGFloat)vValue NS_AVAILABLE_IOS(5_0);
 
 #pragma mark - CIE-LAB
 /// Returns the receiver's lightness (L) component (LAB).
-- (CGFloat)CIELab_LValue;
+- (CGFloat)CIELab_LValue NS_AVAILABLE_IOS(5_0);
 /// Returns one of the receiver's color (a) components (LAB).
-- (CGFloat)CIELab_aValue;
+- (CGFloat)CIELab_aValue NS_AVAILABLE_IOS(5_0);
 /// Returns one of the receiver's color (b) components (LAB).
-- (CGFloat)CIELab_bValue;
+- (CGFloat)CIELab_bValue NS_AVAILABLE_IOS(5_0);
 
 /// Create a color object using the specified CIELAB color component values
 /// @param L The value of the lightness component.
@@ -58,6 +61,6 @@
 /// @param b The value of the b component.
 /// @return The color object. The color information represented by this object is in the device RGB colorspace.
 /// @discussion Even though the CIELab colorspace can handle colors outside the range of RGB, this method converts to RGB for use with UIColor.
-+ (UIColor *)colorWithCIELabL:(CGFloat)L a:(CGFloat)a b:(CGFloat)b;
++ (UIColor *)colorWithCIELabL:(CGFloat)L a:(CGFloat)a b:(CGFloat)b NS_AVAILABLE_IOS(5_0);
 
 @end
