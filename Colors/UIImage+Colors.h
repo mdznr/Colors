1dadc93a04de8bdf0f6c91135ce8d1b5323f0274
Matt Zanchelli <matt@mdznr.com>
2013-11-17 22:42:12 -0500

    Adjusted documentation of methods in UIImage+Colors to be more consistent, accurate and helpful.

--- Colors/UIImage+Colors.h
+++ Colors/UIImage+Colors.h
@@ -14,36 +14,44 @@
 
 /// Describes the level of contrast (perceived difference) between colors.
 typedef NS_ENUM(NSInteger, UIColorContrast) {
-	UIColorContrastLevelLow,    /// Describes a low level of contrast.
-	UIColorContrastLevelMedium, /// Describes a medium level of contrast.
-	UIColorContrastLevelHigh,   /// Describes a high level of contrast.
+	/// Describes a low level of contrast.
+	UIColorContrastLevelLow,
+	/// Describes a medium level of contrast.
+	UIColorContrastLevelMedium,
+	/// Describes a high level of contrast.
+	UIColorContrastLevelHigh,
 } NS_ENUM_AVAILABLE_IOS(5_0);
+
 
 @interface UIImage (Colors)
 
 #pragma mark - Background Color
 
-/// The color that appears to dominate the background of the image.
-/// @discussion Returns nil if no color is found.
+/// The color that appears to dominate the background of the receiving image.
+/// @return A @cUIImage object for the color that appears to dominate the background of the receiving image.
+/// @warning Returns nil if no color is found.
 - (UIColor *)backgroundColor NS_AVAILABLE_IOS(5_0);
 
-/// The color that appears to dominate the background of the image.
+/// The color that appears to dominate the background of the receiving image.
 /// @param colors An array of colors that the returned color needs to contrast with.
 /// @param contrast The perceived difference in color difference.
-/// @discussion Returns nil if no sufficient color is found.
+/// @return A @cUIImage object for the color that appears to dominate the background of the receiving image.
+/// @warning Returns nil if no sufficient color is found.
 - (UIColor *)backgroundColorToContrastAgainstColors:(NSArray *)colors withMinimumContrast:(UIColorContrast)minimumContrast NS_AVAILABLE_IOS(5_0);
 
 
 #pragma mark - Key Color
 
-/// The primary, bright and saturated color that appears to dominate the image.
-/// @discussion Returns nil if no key-color acceptable color is found.
+/// The key color that appears to dominate the receiving image.
+/// @return A @cUIImage object for the key color that appears to dominate the receiving image.
+/// @warning Returns nil if no key-color appropriate color is found.
 - (UIColor *)keyColor NS_AVAILABLE_IOS(5_0);
 
-/// The primary saturated color that appears to dominate the image.
+/// The key color that appears to dominate the receiving image that contasts with specified colors.
 /// @param color An array of colors that the returned color needs to contrast with.
 /// @param contrast The perceived difference in color difference.
-/// @discussion Returns nil if no sufficient key-color acceptable color is found.
+/// @return A @cUIImage object for a sufficient key color that appears to dominate the receiving image.
+/// @warning Returns nil if no sufficient key-color appropriate color is found.
 - (UIColor *)keyColorToContrastAgainstColors:(NSArray *)colors withMinimumContrast:(UIColorContrast)minimumContrast NS_AVAILABLE_IOS(5_0);
 
 @end
