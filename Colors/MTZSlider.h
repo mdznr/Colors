4b6836632a8d7489ea33602a715d2dee7ac834f6
Matt Zanchelli <matt@mdznr.com>
2013-11-17 22:41:11 -0500

    Added some slight documentation to MTZSlider.

--- Colors/MTZSlider.h
+++ Colors/MTZSlider.h
@@ -10,7 +10,14 @@
 
 @interface MTZSlider : UISlider
 
+/**
+ `MTZSlider` is a subclass of `UISlider` with modified artwork.
+ */
+
+///	The image to cover the filled area of the slider (the left side)
 @property (strong, nonatomic) UIImage *fillImage;
+
+///	The image to cover the unfilled area of the slider (the right side)
 @property (strong, nonatomic) UIImage *trackImage;
 
 @end
